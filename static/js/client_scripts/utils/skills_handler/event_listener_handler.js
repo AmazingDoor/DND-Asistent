d63da6e1effeea38780f0a2f0e3e296fbe011e08
AmazingDoor <roystondaniel1@gmail.com>
2025-08-19 04:05:54 -0400

    removed unused files

--- static/js/client_scripts/utils/skills_handler/event_listener_handler.js
+++
@@ -1,8 +0,0 @@
-import {addEventListeners as addAbilityHandlerEventListeners} from './../../ability_handler.js';
-import {addEventListeners as addClassStatsEventListeners} from './../../class_stats_handler.js';
-import {addEventListeners as addRaceEventListeners} from './../race/race_dropdown_handler.js';
-export function addEventListeners(updateSkills, updateModifiers) {
-    addAbilityHandlerEventListeners();
-    addClassStatsEventListeners(updateSkills);
-    addRaceEventListeners(updateSkills, updateModifiers);
-}
