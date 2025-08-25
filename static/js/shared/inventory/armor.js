444d2d6fac7a8aa367cd4caf9fcea1736a27ddef
AmazingDoor <roystondaniel1@gmail.com>
2025-08-25 15:41:06 -0400

    Added armor data

---
+++ static/js/shared/inventory/armor.js
@@ -0,0 +1,100 @@
+export const light_armor = {
+    padded: {
+        cost: "5 gp",
+        ac_mod: 11,
+        strength: 0,
+        stealth_disadvantage: true,
+        weight: 8
+    },
+    leather: {
+        cost: "10 gp",
+        ac_mod: 11,
+        strength: 0,
+        stealth_disadvantage: false,
+        weight: 10
+    },
+    studded_leather: {
+        cost: "45 gp",
+        ac_mod: 12,
+        strength: 0,
+        stealth_disadvantage: false,
+        weight: 13
+    }
+}
+
+export const medium_armor = {
+    hide: {
+        cost: "10 gp",
+        ac_mod: 12,
+        strength: 0,
+        stealth_disadvantage: false,
+        weight: 12
+    },
+    chain_shirt: {
+        cost: "50 gp",
+        ac_mod: 13,
+        strength: 0,
+        stealth_disadvantage: false,
+        weight: 20
+    },
+    scale_mail: {
+        cost: "50 gp",
+        ac_mod: 14,
+        strength: 0,
+        stealth_disadvantage: true,
+        weight: 45
+    },
+    breastplate: {
+        cost: "400 gp",
+        ac_mod: 14,
+        strength: 0,
+        stealth_disadvantage: false,
+        weight: 20
+    },
+    half_plate: {
+        cost: "750 gp",
+        ac_mod: 15,
+        strength: 0,
+        stealth_disadvantage: true,
+        weight: 40
+    }
+}
+
+export const heavy_armor = {
+    ring_mail: {
+        cost: "30 gp",
+        ac_mod: 14,
+        strength: 0,
+        stealth_disadvantage: true,
+        weight: 40
+    },
+    chain_mail: {
+        cost: "75 gp",
+        ac_mod: 16,
+        strength: 13,
+        stealth_disadvantage: true,
+        weight: 55
+    },
+    splint: {
+        cost: "200 gp",
+        ac_mod: 17,
+        strength: 15,
+        stealth_disadvantage: true,
+        weight: 60
+    },
+    plate: {
+        cost: "1500 gp",
+        ac_mod: 18,
+        strength: 15,
+        stealth_disadvantage: true,
+        weight: 65
+    }
+}
+
+export const shield = {
+    cost: "10 gp",
+    ac_mod: 2,
+    strength: 0,
+    stealth_disadvantage: false,
+    weight: 6
+}
